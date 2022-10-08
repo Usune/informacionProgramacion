fca4cbaca49702288aee2f3288420c4eca76bb0a
Usune <101433956+Usune@users.noreply.github.com>
2022-10-08 07:00:10 -0500

    Add files via upload

---
+++ Estefani/cursoGeneralProgramacion/w3schools/curso.js
@@ -0,0 +1,16 @@
+var carName = "Volvo";
+
+//Declarar varibales
+var x = 5;
+var y = 10;
+
+/*
+Cambiar contenido el contenido html
+Uno de los métodos HTML de JavaScript es getElementById()
+El siguiente ejemplo 'busca' un elemento HTML (con id="demo") y cambia el contenido del elemento (innerHTML) a "Hello JavaScript"
+*/
+document.getElementById("demo").innerHTML = "Hello JavaScript";
+
+//Cambiar estilos de HTML
+document.getElementById("demo").style.fontSize = "35px";
+
